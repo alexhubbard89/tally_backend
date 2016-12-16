917db988ee5728d99b9d5b2079ef306184cd5733
alexhubbard89 <alexhubbard89@gmail.com>
2016-12-16 15:17:03 -0500

    add emailing script

---
+++ python/email_test.py
@@ -0,0 +1,26 @@
+import smtplib
+fromaddr = 'tallyscraper@gmail.com'
+toaddrs  = 'alexhubbard89@gmail.com'
+
+try:
+    msg = "\r\n".join([
+            "From: tallyscraper@gmail.com",
+            "To: alexhubbard89@gmail.com",
+            "Subject: All good!",
+            "","You successfully scrapped data!"
+        ])
+except:
+    msg = "\r\n".join([
+            "From: tallyscraper@gmail.com",
+            "To: alexhubbard89@gmail.com",
+            "Subject: Something broke",
+            "","Something about the data scraper went bad :("
+        ])
+username = 'tallyscraper@gmail.com'
+password = os.environ["tallyscraper_password"]
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.ehlo()
+server.starttls()
+server.login(username,password)
+server.sendmail(fromaddr, toaddrs, msg)
+server.quit()
