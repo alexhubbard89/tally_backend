0e212003719e2b3df2b13a4ce33fd14d4ee0b0c7
alexhubbard89 <alexhubbard89@gmail.com>
2016-12-16 10:18:57 -0500

    Heroku test: edit profile

--- app.py
+++ app.py
@@ -25,20 +25,4 @@ def index():
 
 
 if __name__ == '__main__':
-    ## app.run is to run with flask
-    #app.run(debug=True)
-
-    """I should learn why to use tornado and 
-    if it's worth it for us to switch. The
-    code below is to connect to tornado."""
-    from tornado.wsgi import WSGIContainer
-    from tornado.httpserver import HTTPServer
-    from tornado.ioloop import IOLoop
-    import tornado.options
-
-    tornado.options.parse_command_line()
-    http_server = HTTPServer(WSGIContainer(app))
-    port = int(os.environ.get("PORT", 5000))
-    http_server.listen(port=port, address='127.0.0.1')
-    tornado.web.Application(debug=True)
-    IOLoop.instance().start()
+    app.run(debug=True)
