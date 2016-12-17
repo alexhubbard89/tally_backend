6bd4e201a42c3c379f1392cfe7a866476d8b5ac3
alexhubbard89 <alexhubbard89@gmail.com>
2016-12-17 18:39:53 -0500

    fixing merge issues

--- python/collect_current_senate.py
+++ python/collect_current_senate.py
@@ -97,6 +97,7 @@ def put_into_sql(data_set):
         x = list(data_set.loc[i,])
         
         
+        
         for p in [x]:
             format_str = """INSERT INTO current_senate_bio (
             address, 
