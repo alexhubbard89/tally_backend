b0dea23d69dbc9cbf4b2cf98dc0e013383442e85
alexhubbard89 <alexhubbard89@gmail.com>
2016-12-16 19:27:20 -0500

    fix emailer

--- python/data_collector.py
+++ python/data_collector.py
@@ -1,34 +1,30 @@
-import smtplib
 import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import imp
 collect_current_congress = imp.load_source('module', './python/collect_current_congress.py')
 
-
 fromaddr = 'tallyscraper@gmail.com'
-toaddrs  = 'alexhubbard89@gmail.com'
+toaddrs = 'alexhubbard89@gmail.com'
 
+msg = MIMEMultipart('alternative')
+msg['From'] = "tallyscraper@gmail.com"
+msg['To'] = "alexhubbard89@gmail.com"
 try:
     collect_current_congress.collect_current_congress_house()
-    msg = "\r\n".join([
-            "From: tallyscraper@gmail.com",
-            "To: alexhubbard89@gmail.com",
-            "Subject: All good!",
-            "","You successfully scrapped data!"
-        ])
+    msg['Subject'] = "All good!"
+    body = MIMEText("You successfully scrapped data!")
+    msg.attach(body)
 except:
-    msg = "\r\n".join([
-            "From: tallyscraper@gmail.com",
-            "To: alexhubbard89@gmail.com",
-            "Subject: Something broke",
-            "","""Something about the data scraper went bad :(
-            collect_current_congress didn't work"""
-        ])
+    msg['Subject'] = "Something broke"
+    body = MIMEText("""Something about the data scraper went bad :(
+        collect_current_congress didn't work""")
+    msg.attach(body)
+
 username = 'tallyscraper@gmail.com'
 password = os.environ["tallyscraper_password"]
-print "password worked"
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.ehlo()
-server.starttls()
-server.login(username,password)
-server.sendmail(fromaddr, toaddrs, msg)
+server = smtplib.SMTP_SSL('smtp.googlemail.com', 465)
+server.login(username, password)
+server.sendmail(fromaddr, toaddrs, msg.as_string())
 server.quit()
