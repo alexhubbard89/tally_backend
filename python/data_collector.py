c3daa6590e6773918064dd3b699578dc85ae3232
alexhubbard89 <alexhubbard89@gmail.com>
2016-12-16 19:50:29 -0500

    remove try to trouble shoot data collector

--- python/data_collector.py
+++ python/data_collector.py
@@ -11,16 +11,16 @@ toaddrs = 'alexhubbard89@gmail.com'
 msg = MIMEMultipart('alternative')
 msg['From'] = "tallyscraper@gmail.com"
 msg['To'] = "alexhubbard89@gmail.com"
-try:
-    collect_current_congress.collect_current_congress_house()
-    msg['Subject'] = "All good!"
-    body = MIMEText("You successfully scrapped data!")
-    msg.attach(body)
-except:
-    msg['Subject'] = "Something broke"
-    body = MIMEText("""Something about the data scraper went bad :(
-        collect_current_congress didn't work""")
-    msg.attach(body)
+# try:
+collect_current_congress.collect_current_congress_house()
+msg['Subject'] = "All good!"
+body = MIMEText("You successfully scrapped data!")
+msg.attach(body)
+# except:
+#     msg['Subject'] = "Something broke"
+#     body = MIMEText("""Something about the data scraper went bad :(
+#         collect_current_congress didn't work""")
+#     msg.attach(body)
 
 username = 'tallyscraper@gmail.com'
 password = os.environ["tallyscraper_password"]
