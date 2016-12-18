708df2590ff05eb333c4247c5c33b936a3ad46c5
alexhubbard89 <alexhubbard89@gmail.com>
2016-12-17 21:13:06 -0500

    heroku's IP is getting a 403. I'm going to try to senda different payload.

--- python/collect_current_senate.py
+++ python/collect_current_senate.py
@@ -146,8 +146,28 @@ def get_senate_by_gov(df):
     }
     url = 'http://www.senate.gov/general/contact_information/senators_cfm.xml'
     print url
-    r =  requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers)
     print r.status_code
+
+    if r.status_code == 403:
+        payload = {
+            "Host": "www.mywbsite.fr",
+            "Connection": "keep-alive",
+            "Content-Length": 129,
+            "Origin": "https://www.mywbsite.fr",
+            "X-Requested-With": "XMLHttpRequest",
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5",
+            "Content-Type": "application/json",
+            "Accept": "*/*",
+            "Referer": "https://www.mywbsite.fr/data/mult.aspx",
+            "Accept-Encoding": "gzip,deflate,sdch",
+            "Accept-Language": "fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4",
+            "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+            "Cookie": "ASP.NET_SessionId=j1r1b2a2v2w245; GSFV=FirstVisit=; GSRef=https://www.google.fr/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&ved=0CHgQFjAA&url=https://www.mywbsite.fr/&ei=FZq_T4abNcak0QWZ0vnWCg&usg=AFQjCNHq90dwj5RiEfr1Pw; HelpRotatorCookie=HelpLayerWasSeen=0; NSC_GSPOUGS!TTM=ffffffff09f4f58455e445a4a423660; GS=Site=frfr; __utma=1.219229010.1337956889.1337956889.1337958824.2; __utmb=1.1.10.1337958824; __utmc=1; __utmz=1.1337956889.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)"
+        }
+        # Adding empty header as parameters are being sent in payload
+        headers = {}
+        r = requests.post(url, data=dumps(payload), headers=headers)
 
     x = ElementTree.fromstring(r.content)
     x = bf.data(x)
