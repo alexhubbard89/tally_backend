090092b50d5d47b02f35138d50da87783badb95c
alexhubbard89 <alexhubbard89@gmail.com>
2016-12-18 20:08:48 -0500

    troubleshoot bad error

--- python/data_collector.py
+++ python/data_collector.py
@@ -27,8 +27,8 @@ to_collect_or_not_collect = collect_current_senate.get_senator_info()
 good_collection += """
 Current Senate: {}""".format(to_collect_or_not_collect)
 # except:
-# bad_collection += """
-# Current Senate"""
+#     bad_collection += """
+#     Current Senate"""
 
 msg['Subject'] = "Data Collection Report"
 body_msg = """Data Collection Report
