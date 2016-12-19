abf12993909b386a2fe461a66a375b8c96261120
alexhubbard89 <alexhubbard89@gmail.com>
2016-12-18 20:52:22 -0500

    trouble shoot current congress

--- python/collect_current_congress.py
+++ python/collect_current_congress.py
@@ -287,6 +287,7 @@ def collect_current_congress_house():
     
     print 'getting data 1'
     df, status_code_int = get_congress_by_gov()
+    print status_code
     if status_code_int == 403:
         return "But it got a status code of 403 Forbidden HTTP"
     else:
